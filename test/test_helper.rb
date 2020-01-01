160c8629ba2eb526139679bccb5a88f8c6ba55cc
Matt Brictson <matt@mattbrictson.com>
2020-01-01 10:58:19 -0800

    Fix rubocop 0.78.0 issue

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,6 +2,6 @@ $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "bundleup"
 
 # Load everything else from test/support
-Dir[File.expand_path("support/**/*.rb", __dir__)].each { |rb| require(rb) }
+Dir[File.expand_path("support/**/*.rb", __dir__)].sort.each { |rb| require(rb) }
 
 require "minitest/autorun"
